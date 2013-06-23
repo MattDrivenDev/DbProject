9b8f0a1b89b35fb7e21e43fd5a05062cffcce6a9
saxonmatt <saxonmatt@gmail.com>
2013-06-23 23:29:26 +0100

    No need for the develop cmd.

--- release.cmd
+++ release.cmd
@@ -1,8 +1,19 @@
 @ECHO OFF
 
-ECHO ENSURING RELEASE DIRECTORY EXISTS...
+ECHO ENSURING MASTER BRANCH...
+IF NOT EXIST "_develop" (
+	git clone "git@github.com:saxonmatt/DbProject.git" "_develop"
+	cd "_develop"
+	git checkout master
+	cd ..
+)
+
+ECHO ENSURING RELEASE BRANCH...
 IF NOT EXIST "_release" (
-	MKDIR "_release"
+	git clone "git@github.com:saxonmatt/DbProject.git" "_release"
+	cd "_release"
+	git checkout release
+	cd ..
 )
 
 ECHO GENERATING DATABASE RELEASE SCRIPTS...
